8922d7f6633da7f7fa56aab0e6224a5ed7dc8c1f
Saleem Ahmed <saleemkhanx001@gmail.com>
2026-03-03 11:34:56 +0500

    feat(auth): add JWT authentication
    - Implemented JWT token generation service
    - Added user login endpoint `/login`
    - Created protected endpoint `/secure-data`
    - Integrated TokenService

---
+++ Controllers/UserController.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+using Microsoft.AspNetCore.Authorization;
+
+[ApiController]
+[Route("[controller]")]
+
+public class UserController : ControllerBase
+{
+  private readonly TokenService _tokenService;
+
+  private static readonly List<User> _users = new List<User>
+  {
+    new User { UserName = "testuser", Password = "testpassword" }
+  };
+
+  public UserController(TokenService tokenService)
+  {
+    _tokenService = tokenService;
+  }
+
+  [HttpPost("login")]
+  public IActionResult Login([FromBody] User user)
+  {
+    var existingUser = _users.SingleOrDefault(u => u.UserName == user.UserName && u.Password == user.Password);
+    if (existingUser == null)
+    {
+      return Unauthorized("Invalid Username or Password");
+    }
+
+    var token = _tokenService.GenerateToken(user.UserName);
+    return Ok(new { Token = token });
+  }
+
+  [HttpGet("secure-data")]
+  [Authorize]
+  public IActionResult GetSecureData()
+  {
+    return Ok(new { Message = "This is a protected endpoint!" });
+  }
+};
